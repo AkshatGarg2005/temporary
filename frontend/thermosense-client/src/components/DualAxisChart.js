b48f18fd59a94d78e1a2d88a3049097de9a7b20b
Akshat Garg <akshat.23bce10641@vitbhopal.ac.in>
2025-07-24 12:17:31 +0530

    Initial commit

---
+++ frontend/thermosense-client/src/components/DualAxisChart.js
@@ -0,0 +1,49 @@
+import {
+  LineChart,
+  Line,
+  XAxis,
+  YAxis,
+  Tooltip,
+  Legend,
+  ResponsiveContainer,
+} from "recharts";
+
+/**
+ * data = [{ name: 'Now', battery: 40, ambient: 35 }]
+ */
+export default function DualAxisChart({ data }) {
+  return (
+    <div className="chart-wrapper">
+      <ResponsiveContainer width="100%" height={300}>
+        <LineChart data={data}>
+          <XAxis dataKey="name" />
+          <YAxis
+            yAxisId="left"
+            label={{ value: "Battery °C", angle: -90, position: "insideLeft" }}
+          />
+          <YAxis
+            yAxisId="right"
+            orientation="right"
+            label={{ value: "Ambient °C", angle: -90, position: "insideRight" }}
+          />
+          <Tooltip />
+          <Legend />
+          <Line
+            yAxisId="left"
+            type="monotone"
+            dataKey="battery"
+            stroke="#ff7300"
+            dot={{ r: 6 }}
+          />
+          <Line
+            yAxisId="right"
+            type="monotone"
+            dataKey="ambient"
+            stroke="#387908"
+            dot={{ r: 6 }}
+          />
+        </LineChart>
+      </ResponsiveContainer>
+    </div>
+  );
+}
